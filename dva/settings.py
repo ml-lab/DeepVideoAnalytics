818952724ee349fd094b49c1a7d660cf87600dd0
akshay bhat <akshayubhat@gmail.com>
2017-01-23 15:51:51 -0500

    switched to Postgres database.

--- dva/settings.py
+++ dva/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'dva.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dvadb',
+        'USER': 'dvauser',
+        'PASSWORD': 'localpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
