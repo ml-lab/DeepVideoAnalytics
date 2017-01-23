27c5e6b3811de6a2316f0345486d430b887fc1fe
akshay bhat <akshayubhat@gmail.com>
2017-01-23 16:08:19 -0500

    switched to Postgres database.

--- dvaapp/admin.py
+++ dvaapp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Video,Frame,Dataset,Detection,TEvent
 
-# Register your models here.
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Frame)
+class FrameAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Detection)
+class DetectionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TEvent)
+class TEventAdmin(admin.ModelAdmin):
+    pass
+
+
